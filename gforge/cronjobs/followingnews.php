9b30da61b1e652e90a0511a82b0a0b80449cc609
todhing <tod_hing@yahoo.com>
2017-04-20 23:55:27 -0700

    following notification
    * following notification update

--- gforge/cronjobs/followingnews.php
+++ gforge/cronjobs/followingnews.php
@@ -49,197 +49,135 @@ if (!cron_create_lock(__FILE__)) {
 // Pause between messages, sec
 $SLEEP = 1;
 
-$all_users=db_query_params ('Select * FROM users where notification_frequency > 0',array());
+/*
+parse_str($argv[2], $_GET);
+$initial = 'h';
+$initial0 = 'i';
+//parse_str($_SERVER['QUERY_STRING'], $_GET);
+if (isset($_GET['initial'])) {
+  $initial = $_GET['initial'];
+}
+if (isset($_GET['initial0'])) {
+  $initial0 = $_GET['initial0'];
+}
+$all_users=db_query_params ("Select * FROM users where notification_frequency > 0 and LOWER(lastname) like '".$initial."%' or LOWER(lastname) like '".$initial0."%'",array());
+//$all_users=db_query_params ("Select * FROM users where notification_frequency > 0 and LOWER(lastname) like '".$initial."%'",array());
+*/
 
 $subj = "Updates from the SimTK projects you follow";
 
 $max = 0;
 $num_sent = 0;
 $users_notif = 0;
+$body = "";
 $today = date("Y-m-d");
+// create footer
+$tail = "<br />==================================================================<br />" ;
+$tail .= 'You received this message because you are following projects on ' . forge_get_config ('forge_name') . '. You may opt out or change the frequency of the news you receive by logging in to ' . forge_get_config ('forge_name') . ' and visiting your <a href="'.util_make_url('/account/settings.php/').'">Account Settings Page</a>.';
+	
 echo "begin cronjob....\n";
-while ($users = db_fetch_array ($all_users)) {
-    $users_notif++;
-    // if notification set - this check is performed in query above.
-    // if > 0, then get list of projects user belongs to
-    //echo "user: " . $users['user_name'] . "\n";
 
-    //$compare_date = new DateTime(date("Y-m-d H:i", strtotime( '-' . $users['notification_frequency'] . ' days' ) ));
-    //$compare_date = new DateTime(date("Y-m-d"));
-	$compare_date = strtotime("-".$users['notification_frequency']. " days");
-	//echo "compare date: " . $compare_date . "\n";
-	//echo "compare date format: " . date("Y-m-d", $compare_date) . "\n";
-	
-	$send_email = 0;
-        // if notification_date is 0 then automatically send email. This will be users initial setting.
-	if (empty($users['notification_date'])) {
-	  // default to notification_frequency minus today's date
-	  $send_email = 1;	  
-	} else {
-	  // check to see if it's time to send email
-	  //if today's date minus frequency equal to last date sent, then send email	
-      //echo "today-frequency: " . date("Y-m-d",$compare_date) . "\n";
-      //echo "notification date: " . date("Y-m-d",$users['notification_date']) . "\n";	  
-	  if (date("Y-m-d",$compare_date) == date("Y-m-d",$users['notification_date'])){
-	    $send_email = 1;
-	  }
+// get news by days
+
+$numdays = array(1,7,14);
+
+foreach ($numdays as $days) {
+  $arrNews = array();
+  $arrNewsID = array();
+  $arrUser = array();
+  $arrNews = getNewsByDays($days,0);
+  $compare_date = strtotime("-".$days. " days");
+
+  echo "\n\nFollowing Email Sent for Users with Notification Frequency: " . $days . " days\n";
+  echo "Include News with Post Date On or After: " . date("m/d/Y",$compare_date) . "\n";
+
+  //echo "count: " . count($arrNews) . "\n";
+  //var_dump($arrNews);
+
+
+  if ($arrNews) {
+
+   foreach ($arrNews as $news) {
+      $arrNewsID[$news['id']]['user_name'] = $news['user_name'];
+	  $arrNewsID[$news['id']]['picture_file'] = $news['picture_file'];
+	  $arrNewsID[$news['id']]['unix_group_name'] = $news['unix_group_name'];
+	  $arrNewsID[$news['id']]['group_name'] = $news['group_name'];
+	  $arrNewsID[$news['id']]['summary'] = $news['summary'];
+	  $arrNewsID[$news['id']]['group_id'] = $news['group_id'];
+	  $arrNewsID[$news['id']]['post_date'] = $news['post_date'];
+	  
+      // get users following the project
+	  $users_following = db_query_params("SELECT * FROM project_follows,users WHERE users.user_name = project_follows.user_name and users.status = 'A' and group_id= ". $news['group_id'] ." AND follows = true AND notification_frequency = ". $days ." AND (notification_date = 0 OR notification_date <= ". $compare_date . ")",array());
+      if ($users_following) {
+	     while ($row = db_fetch_array($users_following)) {
+		     // assign the news id to each userid array
+             $arrUser[$row['email']][] = $news['id'];
+         }
+	  }	 
 	}
 
-   if ($send_email) {
-	//echo "\n---------------";	
-    //echo "User: " . $users['user_name'] . "\n";
-    //echo "Compare date: " . $compare_date . "\n";
-    // add projects user is member of
-    $i = 0;
-    $cntUserProjects = 0;
-    //get the user object based on the user_name in the URL
-    $user = user_get_object_by_name($users['user_name']);
-	$userProjects = getUserProjects($user,$cntUserProjects);
-	$arrNewsByProjects = array();
-
-	// get list of news, project member of and following projects
-    if ($cntUserProjects > 0) {	   
-	   foreach ($userProjects as $proj) {
-		  // get news for each project
-		  
-		  $arrNews = getNewsByProject($proj['group_id'],0);
-		  //echo "groupid: " . $proj['group_id'] . "\n";
-		  foreach ($arrNews as $news) {
-		    $arrNewsByProjects[$i++] = $news;
-		  }
-       }
-	}
-	
-	// check projects user is following
-	$cntProjectsFollowing = 0;
-    $projectsFollowing = getProjectsFollowing($user,$cntProjectsFollowing);
-	
-    if ($cntProjectsFollowing > 0) {
-	   // index $i should be set above
-	   foreach ($projectsFollowing as $proj) {
-		  // get news for each project
-		  $arrNews = getNewsByProject($proj['group_id'],0);
-		  foreach ($arrNews as $news) {
-		    $arrNewsByProjects[$i++] = $news;
-		  }
-       }
-	}
-	
-	// get news date, compare to today's date
-	$arrNewsSend = array();
-	$i = 0;
-        $body_news = "";	
-	foreach ($arrNewsByProjects as $news) {
-	   $news_date = date("Y-m-d H:i" ,$news['post_date']);
-	   //echo "news date: " . strtotime($news_date) . "\n";
-	   // if notification freq less than diff then include news to send
-	   if ($compare_date < strtotime($news_date)) {
-	      // Format HTML News Page to send out
-		  $tmpPicFile = $news['picture_file'];
-		  if ($tmpPicFile == "") {
-			$tmpPicFile = "user_profile.jpg";
-		  }
-		  $body_news .= '<tr><td width="45" valign="top"> <a href="'.util_make_url('/users/') . $news['user_name'] . '"><img height="40" width="40" src="'.util_make_url('/userpics/') . $tmpPicFile .'"></a></td>';
-		  $body_news .= '<td width="500" valign="top"> <strong><a href="'.util_make_url('/plugins/') . 'simtk_news/news_details.php?flag=2&group_id=' . 
-						$news['group_id'] . 
-						'&id='.$news['id'] . 
+   // loop through each userid
+   if ($arrUser) {
+      foreach($arrUser as $email => $value) {
+	     echo "\nSend email to: " . $email . "\r\n";
+		 $body = "";
+		 $body_news = "";
+         foreach ($value as $key2 => $id) {
+            echo "News Summary: ". $arrNewsID[$id]['summary'] ."\r\n";
+			echo "News Post Date: ". date("m/d/Y",$arrNewsID[$id]['post_date']) ."\r\n";
+			// build body of email
+			// Format HTML News Page to send out
+		    $tmpPicFile = $arrNewsID[$id]['picture_file'];
+		    if ($tmpPicFile == "") {
+			  $tmpPicFile = "user_profile.jpg";
+		    }
+		    $body_news .= '<tr><td width="45" valign="top"> <a href="'.util_make_url('/users/') . $arrNewsID[$id]['user_name'] . '"><img height="40" width="40" src="'.util_make_url('/userpics/') . $tmpPicFile .'"></a></td>';
+		    $body_news .= '<td width="500" valign="top"> <strong><a href="'.util_make_url('/plugins/') . 'simtk_news/news_details.php?flag=2&group_id=' . 
+						$arrNewsID[$id]['group_id'] . 
+						'&id='.$id . 
 						'" style="color:#5e96e1;">' . 
 						$news['summary'] . 
 					'</a></strong><br />
 					<small><a href="'.util_make_url('/projects/') . 
-						$news['unix_group_name'] . 
+						$arrNewsID[$id]['unix_group_name'] . 
 						'/">' . 
-						$news['group_name'] . 
+						$arrNewsID[$id]['group_name'] . 
 						'</a> ' . 
-						date(_('M d, Y'),$news['post_date']) . 
+						date(_('M d, Y'),$arrNewsID[$id]['post_date']) . 
 					'</small></td>';
 		  $body_news .= "</tr>";
-		  // store information in an array for later use
-		  $arrNewsSend[$i++] = $news;
-	   } // if compare_date
-	} // foreach
-        if ($arrNewsSend) {
-	   $body = "<p><b>Here's the latest news from the SimTK projects you are following:</b></p><br />";
-	   $body .= '<table cellpadding="5" cellspacing="5">';
-           $body .= $body_news;
-	   $body .= "</table>";
-        }
-	
-	// get related projects
-	$allTroveCat = array();
-	foreach ($projectsFollowing as $proj) {
-		  // get news for each project
-		  $troveCategories = getTroveCategories($proj['group_id']);
-		  //print_r($troveCategories);
-		  $allTroveCat = array_merge($troveCategories,$allTroveCat);
-    }
-	
-	// get most common category
-	$c = array_count_values($allTroveCat);
-	if (!empty($c)) {
-	   $commonCat = array_search(max($c),$c);
-	   $cntProjectsByCat = 0;
-	   //echo "Common Category: " . $commonCat . "\n";
-	   $resultsProjectsCat = getProjectsByCat($cntProjectsByCat,$commonCat,$compare_date);
-	   // add content to body
-	   if ($resultsProjectsCat) {
-	      $max = db_numrows($resultsProjectsCat);
-	      if ($max > 0) {
-	         $body .= "<br /><br /><p><b>Here are related SimTK projects recently created:</b></p><br />";
-	         $body .= "<table>";
-	      } // max > 0
-	      for ($i = 0; $i < $max; $i++) {
-	         $tempLogo = db_result($resultsProjectsCat, $i, 'simtk_logo_file');
-	         if (!empty($tempLogo)) {
-		        $body .= '<tr><td width="45" valign="top"><img height="40" width="40" src="'.util_make_url('/logos/') . db_result($resultsProjectsCat, $i, 'simtk_logo_file') .'"></td>';
-		     } else {
-		        $body .= '<tr><td width="45" valign="top"><img height="40" width="40" src="'.util_make_url('/logos/_thumb') .'"></td>';
-		     }
+			
+			
+         }
+		 $body = "<p><b>Here's the latest news from the SimTK projects you are following:</b></p><br />";
+	     $body .= '<table cellpadding="5" cellspacing="5">';
+         $body .= $body_news;
+	     $body .= "</table>";
+	     // send email
+		 if ($email == "tod_hing@yahoo.com" || $email == "joyku@stanford.edu" || $email == "hykwong@stanford.edu") {
+            util_send_message($email,$subj, $body.$tail,'noreply@'.forge_get_config('web_host'),'','','',true);
+	     }
 		  
-		     $body .= '<td width="500" valign="top"> <strong><a href="'.util_make_url('/projects/') . db_result($resultsProjectsCat, $i, 'unix_group_name') . 
-						'" style="color:#5e96e1;">' . db_result($resultsProjectsCat, $i, 'group_name') . '</a></strong><br /><small>' . 
-						db_result($resultsProjectsCat, $i, 'simtk_summary') . '</small></td>';
-		     $body .= "</tr>";
-	      } // for
-	      if ($max > 0) {
-	         $body .= "</table>";
-	      }  // max > 0
-	   } // if resultsProjectsCat
-	} // if empty $c
-	
-	
-	// create footer
-	$tail = "<br />==================================================================<br />" ;
-	$tail .= 'You received this message because you are following projects on ' . forge_get_config ('forge_name') . '. You may opt out or change the frequency of the news you receive by logging in to ' . forge_get_config ('forge_name') . ' and visiting your <a href="'.util_make_url('/account/settings.php/').'">Account Settings Page</a>.';
-		
-	// send news.
-	if ($arrNewsSend || $max > 0) {
-       util_send_message($user->getEmail(),$subj, $body.$tail,'noreply@'.forge_get_config('web_host'),'','','',true);
-	   echo "Email Sent to User: " . $user->getEmail() . "\n";
-	   $num_sent++;
-	}
+		 // update notification_sent field for user
+	     db_begin();
+	     $sql = "update users set notification_date = " . time() . " where email = '" . $email . "'";
+	     $result_user = db_query_params($sql, array());				
+	     db_commit();
+      } // foreach arrUser
+   } // if arrUser
+   
 
-	// update notification_sent field for user - regardless if any new news items or not
-	db_begin();
-	$sql = 'update users set notification_date = ' . time() . ' where user_id = ' . $users['user_id'];
-	$result_user = db_query_params($sql, array());				
-	db_commit();
-					
-	sleep($SLEEP);
-	
-   } // if send email
-}  // while
+  }
   
-  
-  
+}  
 // send message to admin  
-echo "Emails sent: " . $num_sent . "\n";
-echo "Num Users with notification on: " . $users_notif . "\n";
+//echo "Emails sent (Status: Monitor Only): " . $num_sent . "\n";
+//echo "Num Users with notification on: " . $users_notif . "\n";
 
 if (db_error()) {
 	$err .= $sql.db_error();
 }
-$mess = "following news mail sent";
+$mess = "following news mail sent: " . $num_sent;
 m_exit($mess);
 
 
