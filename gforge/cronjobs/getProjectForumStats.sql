12ed612ff4d266794028814f18fbe1b455cd4f23
hykwong <hykwong@stanford.edu>
2017-09-01 11:45:09 -0700

    Cronjob forum statistics.
    - Updated with checking for topic_visibility and post_visibility to
    account for soft-deletion

--- gforge/cronjobs/getProjectForumStats.sql
+++ gforge/cronjobs/getProjectForumStats.sql
@@ -2,10 +2,10 @@
 
 SELECT phpbb_forums.forum_id, topics.num_threads, posts.num_posts, forum_name FROM phpbb_forums 
 JOIN 
-(SELECT COUNT(topic_id) num_threads, forum_id FROM phpbb_topics GROUP BY phpbb_topics.forum_id) topics 
+(SELECT COUNT(topic_id) num_threads, forum_id FROM phpbb_topics WHERE topic_visibility=1 GROUP BY phpbb_topics.forum_id) topics 
 ON phpbb_forums.forum_id=topics.forum_id 
 JOIN 
-(SELECT COUNT(post_id) num_posts, forum_id FROM phpbb_posts GROUP BY phpbb_posts.forum_id) posts 
+(SELECT COUNT(post_id) num_posts, forum_id FROM phpbb_posts WHERE post_visibility=1 GROUP BY phpbb_posts.forum_id) posts 
 ON phpbb_forums.forum_id=posts.forum_id 
 WHERE phpbb_forums.forum_id IN 
 (SELECT forum_id FROM phpbb_posts WHERE post_visibility=1 GROUP BY forum_id) 
