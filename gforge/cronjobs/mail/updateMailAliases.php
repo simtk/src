fbd4384846f2ab83b14b8ba8cf77248c62bf0ae0
hykwong <henryadmin@garnier.stanford.edu>
2018-01-10 17:20:27 -0800

    Missing mailing lists in /etc/aliases.
    - Fixed /etc/aliases by including the missing mailing lists sections
    that should be present.
    
    Fixed:
    https://simtk.org/tracker/index.php?func=detail&aid=2448&group_id=11&ati
    d=1960
    https://simtk.org/tracker/index.php?func=detail&aid=2585&group_id=11&ati
    d=1960

---
+++ gforge/cronjobs/mail/updateMailAliases.php
@@ -0,0 +1,165 @@
+#! /usr/bin/php
+<?php
+
+/**
+ *
+ * updateMailAliases.php
+ * 
+ * Update mailing list aliases file. 
+ * 
+ * Copyright 2005-2018, SimTK Team
+ *
+ * This file is part of the SimTK web portal originating from        
+ * Simbios, the NIH National Center for Physics-Based               
+ * Simulation of Biological Structures at Stanford University,      
+ * funded under the NIH Roadmap for Medical Research, grant          
+ * U54 GM072970, with continued maintenance and enhancement
+ * funded under NIH grants R01 GM107340 & R01 GM104139, and 
+ * the U.S. Army Medical Research & Material Command award 
+ * W81XWH-15-1-0232R01.
+ * 
+ * SimTK is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * SimTK is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with SimTK. If not, see  
+ * <http://www.gnu.org/licenses/>.
+ */ 
+ 
+require dirname(__FILE__) . '/../../www/env.inc.php';
+require_once $gfcommon . 'include/pre.php';
+
+// Find all active mailing lists.
+$arrMissingMailList = array();
+$strQuery = "SELECT list_name FROM mail_group_list " .
+	"WHERE status=3";
+$res = db_query_params($strQuery, array());
+$numRows = db_numrows($res);
+for ($cnt = 0; $cnt < $numRows; $cnt++) {
+	$strMailList = strtolower(db_result($res, $cnt, 'list_name'));
+	if (findMailListAddr($strMailList) === FALSE) {
+		// Mailing list is not in /etc/aliases file.
+		// Remember this missing list.
+		$arrMissingMailList[] = $strMailList;
+	}
+}
+
+if (count($arrMissingMailList) > 0) {
+
+	// Has missing mailing lists.
+	// Make a copy of /etc/aliases file first.
+	copy("/etc/aliases", "/etc/aliases_" . date('Y_m_d_H_i'));
+
+	$strMissing = "";
+	$strEmail = "The following mailing lists were missing from /etc/aliases. Fixed.<br/>" . PHP_EOL;
+	foreach ($arrMissingMailList as $strMailList) {
+		$strEmail .= $strMailList . "<br/>" . PHP_EOL;
+
+		$strMissing .= $strMailList . ":" . 
+			' 		 "|/usr/lib/mailman/mail/mailman post ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-admin:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman admin ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-bounces:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman bounces ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-confirm:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman confirm ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-join:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman join ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-leave:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman leave ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-owner:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman owner ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-request:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman request ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-subscribe:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman subscribe ' . $strMailList . '"' . PHP_EOL;
+		$strMissing .= $strMailList . "-unsubscribe:" . 
+			' 		 "|/usr/lib/mailman/mail/mailman unsubscribe ' . $strMailList . '"' . PHP_EOL;
+	}
+
+	// NOTE: Put the missing aliases to the line after "mailman-unsubscribe:"
+	// such that these aliases are among the mailing lists section, 
+	// i.e. somewhere in the middle of the /etc/aliases file.
+	// Do not append to the end of /etc/aliases, because there 
+	// are #GFORGEBEGIN and #GFORGEEND markers; it is safer to 
+	// put the missing section to among the mailing lists and also
+	// it'll be easier to find these missing mailing lists.
+	$strHead = "";
+	$strTail = "";
+	$startTail = FALSE;
+
+	$fh = fopen("/etc/aliases", "r");
+	while (($line = fgets($fh)) !== false) {
+		if ($startTail === FALSE) {
+			// Get head.
+			$strHead .= $line;
+		}
+		else {
+			// Get tail.
+			$strTail .= $line;
+		}
+
+		// Put lines after the line that starts with 
+		// "mailman-subscribe:" to the tail section.
+		$idx = strpos($line, "mailman-unsubscribe:");
+		if ($idx === 0) {
+			// Next line with go to tail section.
+			$startTail = TRUE;
+		}
+	}
+
+	//echo $strMissing;
+
+	// Embed missing aliases between the head and tail sections.
+	$strBuff = $strHead . $strMissing . $strTail;
+
+	// Add missing list(s) contents to /etc/aliases.
+	//echo $strBuff;
+	file_put_contents("/etc/aliases", $strBuff, LOCK_EX);
+
+	// Send notification email about missing lists.
+	sendEmail("webmaster@simtk.org", "Fixed missing mailing list", $strEmail);
+}
+
+// Send email.
+function sendEmail($theEmailAddr, $theTitle, $theMsgBody) {
+
+	$headers[] = 'MIME-Version: 1.0';
+	$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+	$headers[] = 'From: root@simtk.org';
+
+	mail($theEmailAddr, $theTitle, $theMsgBody, implode("\r\n", $headers));
+}
+
+// Search for presence of mailing list in /etc/aliases file.
+function findMailListAddr($strMailList) {
+
+	// Append ":" to the mailing list name for searching.
+	$strMailList .= ":";
+
+	$foundit = false;
+	$fh = fopen("/etc/aliases", "r");
+
+	// Look up each line.
+	while (($buff = fgets($fh)) !== false) {
+		$idx = strpos($buff, $strMailList);
+		// Has to be at beginning of line.
+		if ($idx === 0) {
+			$foundit = TRUE;
+			break;
+		}
+	}
+
+	fclose($fh);
+
+	return $foundit;
+}
+
+?>
