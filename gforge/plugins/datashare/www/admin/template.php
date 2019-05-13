657cc1d38551e19176dcf93826b3a17c8fe2a22f
todhing <tod_hing@yahoo.com>
2019-05-13 16:37:04 -0700

    fixes to datashare errors
    * path to files caused warnings in apache log file.  Fixed in v3.0.3.

--- gforge/plugins/datashare/www/admin/template.php
+++ gforge/plugins/datashare/www/admin/template.php
@@ -2,36 +2,36 @@
 /**
  *
  * datashare plugin template.php
- * 
+ *
  * admin page for creating new study.
  *
  * Copyright 2005-2016, SimTK Team
  *
- * This file is part of the SimTK web portal originating from        
- * Simbios, the NIH National Center for Physics-Based               
- * Simulation of Biological Structures at Stanford University,      
- * funded under the NIH Roadmap for Medical Research, grant          
+ * This file is part of the SimTK web portal originating from
+ * Simbios, the NIH National Center for Physics-Based
+ * Simulation of Biological Structures at Stanford University,
+ * funded under the NIH Roadmap for Medical Research, grant
  * U54 GM072970, with continued maintenance and enhancement
- * funded under NIH grants R01 GM107340 & R01 GM104139, and 
- * the U.S. Army Medical Research & Material Command award 
+ * funded under NIH grants R01 GM107340 & R01 GM104139, and
+ * the U.S. Army Medical Research & Material Command award
  * W81XWH-15-1-0232R01.
- * 
+ *
  * SimTK is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as 
+ * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * SimTK is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public 
- * License along with SimTK. If not, see  
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with SimTK. If not, see
  * <http://www.gnu.org/licenses/>.
- */ 
+ */
 
-require_once $gfplugins.'env.inc.php';
+require_once '../../../env.inc.php';
 require_once $gfcommon.'include/pre.php';
 require_once '../datashare-utils.php';
 require_once $gfplugins.'datashare/include/Datashare.class.php';
@@ -61,7 +61,7 @@ if (session_loggedin()) {
 	echo "</div>";
 	constructSideBar($group);
 	echo "</div></div>";
-	
+
 	datashare_footer(array());
 
 } else {
