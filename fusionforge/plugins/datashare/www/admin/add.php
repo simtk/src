5ffc433b7d3ffa59735498333f3c1c52e8a1c18a
HK <hykwong@stanford.edu>
2020-07-20 16:01:37 -0700

    [data share] License support; archive upload. - Added support of use of license. - Aded warning of potential folders overwriting when uploading archive. - Updated handling of anonymous user. - Enhanced downloads of file, release, and query results. - Enhanced handshake performance between DataShare and SimTK servers. - Updated parameter checks.
    Fixed:
    https://simtk.org/tracker/index.php?func=detail&aid=2953&group_id=11&atid=1960
    https://simtk.org/tracker/index.php?func=detail&aid=2906&group_id=11&atid=1960

--- fusionforge/plugins/datashare/www/admin/add.php
+++ fusionforge/plugins/datashare/www/admin/add.php
@@ -81,6 +81,8 @@ $subject_prefix = getStringFromRequest('subject_prefix');
 if (trim($subject_prefix) == "") {
 	$subject_prefix = "subject";
 }
+$useAgreement = getIntFromRequest('use_agreement');
+$customAgreement = trim(getStringFromRequest('license_preview'));
 
 if (session_loggedin()) {
 
@@ -105,18 +107,32 @@ if (session_loggedin()) {
 		exit_error($study->getErrorMessage(), 'Datashare Error');
 	}
 
-	$templates = $study->getTemplate();
+	$exec_changes = true;
+	if ($useAgreement !== 0) {
+		if (stripos($customAgreement, "[Insert Year(s)], [Insert organization or names of copyright holder(s)]") !== FALSE) {
+			$exec_changes = false;
+			$warning_msg = "Please update copyright years and organization or names of copyright holder(s) in license agreement";
+		}
+		else if (stripos($customAgreement, "[Insert Year(s)]") !== FALSE) {
+			$exec_changes = false;
+			$warning_msg = "Please update copyright year(s) in license agreement";
+		}
+		else if (stripos($customAgreement, "[Insert organization or names of copyright holder(s)]") !== FALSE) {
+			$exec_changes = false;
+			$warning_msg = "Please update organization or names of copyright holder(s) in license agreement";
+		}
+	}
 
-
-	if (getStringFromRequest('post_changes')) {
+	if (getStringFromRequest('post_changes') && $exec_changes) {
 		if (!form_key_is_valid(getStringFromRequest('form_key'))) {
 			exit_form_double_submit('datashare');
 		}
 
 		// add new study
 		if ($insert_result = $study->insertStudy($group_id, $title, $description, 
-			$is_private, $template, $subject_prefix)) {
-			$feedback = _('Study Added');
+			$is_private, $template, 
+			$subject_prefix, $useAgreement, $customAgreement)) {
+			$feedback = 'Study Added';
 		}
 		else {
 			form_release_key(getStringFromRequest('form_key'));
@@ -138,9 +154,23 @@ if (session_loggedin()) {
 
 ?>
 
+
+<script src='/frs/download.js'></script>
+<link rel='stylesheet' href='/frs/download.css' type='text/css' />
+<script src='/frs/admin/preload_license.js'></script>
+<script src='/frs/admin/license.js'></script>
+
 <script>
         // Update flag input components after document has been loaded completely.
         $(document).ready(function() {
+		$(".use_agreement").click(function() {
+			if ($(this).val() == 0) {
+				$(".license_preview").hide();
+			}
+			else {
+				$(".license_preview").show();
+			}
+		});
 
 <?php
 	// Flag components that have errors.
@@ -192,15 +222,6 @@ if (session_loggedin()) {
 
 	echo '<p>Using Top Folder Template (default)</p>';
 
-	/*
-	echo '<select name="template">';
-	foreach ($templates as $template) {
-		echo "<option value='".$template->template_id."'>".$template->title."</option>";
-	}
-	echo '</select>';
-	echo '<br /><br />';
-	*/
-
 	echo "<input type='hidden' name='template' value='1'>";
 
 	echo '<p>';
@@ -219,6 +240,74 @@ if (session_loggedin()) {
 	echo "<p><input type=\"radio\" name=\"is_private\" value=\"1\"> Registered User - only SimTK members who are logged into SimTK can view and download the data.";
 	echo "<p><input type=\"radio\" name=\"is_private\" value=\"2\"> Private - only designated members of the project can view and download the data.";
 
+?>
+
+<br/>
+<br/>
+
+<strong>License agreement:</strong>
+<a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="You can display a download agreement to users before they download any file in this package. The option to show this agreement can be removed for a given file when that file is added to the package.<br/><br/>
+
+For code, we recommend that you add a license agreement as a comment header in every source file. This is in addition to or instead of this download agreement. A summary of each license is provided, but you should consult the license itself for the exact terms that apply.">?</a>
+
+<br/>
+
+<div style="margin-left:20px;">
+
+<strong><label>Open Source Licenses</label></strong>
+<a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="
+* Allows others to use your data/documentation/software for any purpose, commercial or non-commercial, make modifications, and redistribute it.<br/>
+* Includes a disclaimer of warranty.<br/>
+* Users are obligated to include your license terms if they redistribute. See <a href='http://www.opensource.org' target='_blank'>http://www.opensource.org</a> and <a href='http://en.wikipedia.org/wiki/Comparison_of_free_software_licenses' target='_blank'>Wikipedia</a> for more information.<br/>
+* SimTK provides some common open-source licenses to choose from.  Many other options exist. See <a href='https://spdx.org/licenses/' target='_blank'>list of other open-source licenses</a>.<br/><br/>
+
+These licenses differ in the additional obligations they place on the users.
+">?</a>
+
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="2" checked="checked"> MIT <a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Default license. No additional obligations.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="3"> LGPL <a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Modifications that are redistributed must include the modified source under the same terms.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="4"> GPL </input><a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Any distributed work that includes all or part of GPL-licensed material must itself be offered under GPL, meaning that all the source code is available.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="6"> CC BY 4.0 </input><a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Others can share and adapt the file(s) for any purpose, even commercially, but they must give proper attribution.  Similar to MIT license but applies to works beyond just software and related documentation.  Also, it provides more terms and conditions.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="7"> Apache 2.0 </input><a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Similar to MIT license. Some major differences: 1) Apache 2.0 offers more explicit patent protection and 2) it also requires listing all modifications to original software.">?</a>
+</p>
+
+<strong><label>Other licenses</label></strong>
+<a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content='Many other licenses can be used. See <a href="http://en.wikipedia.org/wiki/Comparison_of_free_software_licenses" target="_blank">Wikipedia</a> and <a href="http://creativecommons.org/licenses" target="_blank">Creative Commons</a>. For complex licenses, we recommend that you enter a URL for the license, e.g., "The [project name] license agreement can be read here: http://XXX."'>?</a>
+
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="5"> Creative Commons Attribution-Non-Commercial </input><a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Similar to open-source licenses except your work can only be used for non-commercial purposes.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="1"> Custom </input><a href="javascript://" class="popoverLic" data-html="true" data-toggle="popover" data-placement="right" data-content="Specify a custom license.">?</a>
+</p>
+<p>
+<input type="radio" name="use_agreement" class="use_agreement" value="0"> None </input>
+</p>
+
+<br/>
+
+<span class="edit_notice" style="color:#f75236;">Update first line of license with (1) copyright year and (2) organization or copyright holder</span>
+
+<br/>
+
+<input type="hidden" class="custom_license" value="" /> <textarea class="license_preview" style="margin-top:5px;" rows="10" cols="50" name="license_preview" title="Preview license">
+</textarea>
+
+</div>
+
+<br/>
+<br/>
+
+<?php
 	echo '<div><input type="submit" name="submit" value="'._('Submit').'" class="btn-cta" /></div></div></form>';
 
 	echo "</div>";
---
+++ fusionforge/plugins/datashare/www/userLogin.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * FusionForge login page
+ *
+ * This is main login page. It takes care of different account states
+ * (by disallowing logging in with non-active account, with appropriate
+ * notice).
+ *
+ * Copyright 1999-2001 (c) VA Linux Systems
+ * Copyright 2011, Roland Mas
+ * Copyright 2011, Franck Villaume - Capgemini
+ * Copyright 2016-2020, SimTK Team
+ *
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+Header("Expires: Wed, 11 Nov 1998 11:11:11 GMT");
+Header("Cache-Control: no-cache");
+Header("Cache-Control: must-revalidate");
+
+require_once '../../env.inc.php';
+require_once $gfcommon .'include/pre.php';
+
+global $warning_msg;
+
+$typeConfirm = getIntFromRequest('typeConfirm');
+$group_id = getIntFromRequest('groupid');
+$study_id = getIntFromRequest('studyid');
+$nameDownload = trim(getStringFromRequest('nameDownload'));
+// Validate pathname for download.
+if (!empty($nameDownload) &&
+	(!empty(preg_replace("/[-A-Z0-9+_\. ~\/]/i", "", $nameDownload)) ||
+	strstr($nameDownload, ".."))) {
+	$warning_msg = "Invalid filename for download.";
+	$HTML->header(array('title'=>'Login'));
+	$HTML->footer(array());
+	exit;
+}
+
+$login = getStringFromRequest('login');
+$skip = getStringFromRequest('skip');
+$form_loginname = getStringFromRequest('form_loginname');
+$form_pw = getStringFromRequest('form_pw');
+
+$plugin = plugin_get_object('authbuiltin');
+
+if (forge_get_config('use_ssl') && !session_issecure()) {
+	// Force use of SSL for login.
+	header('Location: https://' . getStringFromServer('HTTP_HOST') . getStringFromServer('REQUEST_URI'));
+}
+
+// Check for valid login; if validatd, redirect.
+if ($login == "Login") {
+	// Log out user first.
+	session_logout();
+
+	$urlView = "view.php?plugin=datashare&" .
+		"id=$group_id&studyid=$study_id&typeConfirm=$typeConfirm";
+	if ($nameDownload != "") {
+		// File download. Add file name.
+		$urlView .= "&nameDownload=" . $nameDownload;
+	}
+
+	if (trim($form_loginname) == "" || trim($form_pw) == "") {
+		$warning_msg = "Missing Password Or User Name";
+	}
+	else if (session_check_credentials_in_database(strtolower($form_loginname), $form_pw, false)) {
+		if ($plugin->isSufficient()) {
+			$plugin->startSession($form_loginname);
+		}
+		// User validated.
+		header("Location: $urlView");
+		exit;
+	}
+	else {
+		$warning_msg = "Invalid Password Or User Name";
+	}
+}
+else if ($skip == "Skip") {
+	$urlView = "view.php?plugin=datashare&" .
+		"id=$group_id&studyid=$study_id&typeConfirm=$typeConfirm";
+	if ($nameDownload != "") {
+		// File download. Add file name.
+		$urlView .= "&nameDownload=" . $nameDownload;
+	}
+
+	header("Location: $urlView");
+	exit;
+}
+
+
+$HTML->header(array('title'=>'Login'));
+
+
+// Otherwise, display the login form again
+
+?>
+
+<script>
+	$(document).ready(function () {
+		$(".loginname").focus();
+	});
+</script>
+
+
+<h2>Please login if you have a SimTK account.</h2>
+<form action="/plugins/datashare/userLogin.php" method="post">
+<?php
+	if ($nameDownload != "") {
+		echo '<input type="hidden" name="nameDownload" value="' . $nameDownload . '">';
+	}
+	echo '<input type="hidden" name="groupid" value="' . $group_id . '">';
+	echo '<input type="hidden" name="studyid" value="' . $study_id . '">';
+	echo '<input type="hidden" name="typeConfirm" value="' . $typeConfirm . '">';
+?>
+	<p>Login Name:<br/>
+	<input type="text" class="loginname" name="form_loginname" value="" >
+	</p>
+	<p>Password:<br/>
+	<input type="password" name="form_pw" ></p>
+	<p><input type="submit" class="btn-cta" name="login" value="Login">
+	<input type="submit" class="btn-cta" name="skip" value="Skip"></p>
+</form>
+
+<?php
+
+$HTML->footer(array());
+
+?>
+
