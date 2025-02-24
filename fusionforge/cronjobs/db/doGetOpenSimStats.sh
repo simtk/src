f529f50f0e231fed06255cc3592281b6821c6aab
HK <hykwong@stanford.edu>
2025-02-24 15:14:44 -0800

    cronjobs - Updated stats cronjobs.

--- fusionforge/cronjobs/db/doGetOpenSimStats.sh
+++ fusionforge/cronjobs/db/doGetOpenSimStats.sh
@@ -12,6 +12,6 @@ psql -h${db_server} -U${db_user} -d${db_name_ff} -f ${FILENAME} >| statsOpenSim.
 FILENAME=/usr/share/gforge/cronjobs/db/getOpenSimForumStats.sql
 psql -h${db_server} -U${db_user} -d${db_name_forum} -f ${FILENAME} >> statsOpenSim.txt
 
-#for x in rcvStats@simtk.org ; do
-#    echo 'See attached file.' | /usr/bin/mutt -a ${BASE_FILENAME}.txt -s "$SUBJECT" -- $x
-#done
+for x in rcvStats@simtk.org ; do
+    echo 'See attached file.' | /usr/bin/mutt -a ${BASE_FILENAME}.txt -s "$SUBJECT" -- $x
+done
