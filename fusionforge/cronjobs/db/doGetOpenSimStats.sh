5f335b072eb66395f81371d17cb87b6e1e9e8370
HK <hykwong@stanford.edu>
2025-03-05 13:37:35 -0800

    cronjobs - Updated stats cronjobs.

--- fusionforge/cronjobs/db/doGetOpenSimStats.sh
+++ fusionforge/cronjobs/db/doGetOpenSimStats.sh
@@ -5,13 +5,14 @@ if test -f /usr/share/gforge/cronjobs/db/cronjobs.ini ; then
 fi
 
 SUBJECT=`hostname`": OpenSim downloads and forum stats"
+DIRNAME=/usr/share/gforge/cronjobs/db
 
-FILENAME=/usr/share/gforge/cronjobs/db/getOpenSimDownloadStats.sql
-psql -h${db_server} -U${db_user} -d${db_name_ff} -f ${FILENAME} >| statsOpenSim.txt
+FILENAME=getOpenSimDownloadStats.sql
+psql -h${db_server} -U${db_user} -d${db_name_ff} -f ${DIRNAME}/${FILENAME} >| ${DIRNAME}/statsOpenSim.txt
 
-FILENAME=/usr/share/gforge/cronjobs/db/getOpenSimForumStats.sql
-psql -h${db_server} -U${db_user} -d${db_name_forum} -f ${FILENAME} >> statsOpenSim.txt
+FILENAME=getOpenSimForumStats.sql
+psql -h${db_server} -U${db_user} -d${db_name_forum} -f ${DIRNAME}/${FILENAME} >> ${DIRNAME}/statsOpenSim.txt
 
 for x in rcvStats@simtk.org ; do
-    echo 'See attached file.' | /usr/bin/mutt -a ${BASE_FILENAME}.txt -s "$SUBJECT" -- $x
+    echo 'See attached file.' | /usr/bin/mutt -a ${DIRNAME}/statsOpenSim.txt -s "$SUBJECT" -- $x
 done
